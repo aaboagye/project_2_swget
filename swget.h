74a869d1d46dc86b70fc403c9a6bf9fb3d3ad746
tanCelena <tan.celena@gmail.com>
2012-10-31 11:23:53 -0700

    Nothing changed, just added top comments

--- swget.h
+++ swget.h
@@ -1,3 +1,11 @@
+/* Name & E-mail: Celena Tan            tan.celena@gmail.com
+ * Name & E-mail: Aseda Aboagye         aseda.aboagye@gmail.com
+ * File Name: swget.h
+ * Course: COMP 177 Computer Networking
+ * Project: swget
+ * Created on: October 24, 2012
+ * Last Edited: October 26, 2012 */
+
 #ifndef SWGET_H
 #define SWGET_H
 #include <argp.h>
@@ -17,5 +25,7 @@ struct host_info {
 };
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state);
+
 static void parse_url(char *src_url, struct host_info *h);
+
 #endif
