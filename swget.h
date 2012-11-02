c81eb01634244aa8d2a5cdd19b585aa012d0805d
tanCelena <tan.celena@gmail.com>
2012-11-01 23:00:20 -0700

    Updated again =)

--- swget.h
+++ swget.h
@@ -1,11 +1,3 @@
-/* Name & E-mail: Celena Tan            tan.celena@gmail.com
- * Name & E-mail: Aseda Aboagye         aseda.aboagye@gmail.com
- * File Name: swget.h
- * Course: COMP 177 Computer Networking
- * Project: swget
- * Created on: October 24, 2012
- * Last Edited: November 1, 2012 */
-
 #ifndef SWGET_H
 #define SWGET_H
 #include <argp.h>
@@ -18,9 +10,9 @@ struct arguments {
 
 struct host_info {
     char *host;
+    char *ip;
     int port;
     char *path;
-    int is_ftp;
     char *user;
 };
 
@@ -28,8 +20,10 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state);
 
 static void parse_url(char *url, struct host_info *h);
 
-//static void name_resolution(char *url, struct host_info *h); <--- Not needed anymore
+int parse_response(char *response);
 
-static void handle_redirect(char *url, struct host_info *h);
+int parse_header(char *response);
+
+static void handle_redirects(char *url, struct host_info *h);
 
 #endif
